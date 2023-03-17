913bba4787cfd23c3981de1da4d1125eaa11c01a
Vartika Singh <vartikatomar270@gmail.com>
2023-03-17 19:36:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const port = 8080
+
+const refreshAll = require('./createDatabase')
+refreshAll() ;
+const router = require('./Routes/covidTally')
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//const { connection } = require('./connector')
+app.use(router);
+
+
+
+
+
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app;
